d647407d9c41bb6612d42a851501562fea9a2c3c
walbermr <wmr@cin.ufpe.br>
2019-06-21 16:40:34 -0300

    All robots tested, added visualization for motors

--- preprocessing.py
+++ preprocessing.py
@@ -4,13 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    list_f_str = ['robot-0', 'robot-1', 'robot-2', 'robot-5', 'robot-10']
+    list_f_str = ['robot-0', 'robot-1', 'robot-2', 'robot-3', 'robot-4', 'robot-5', 'robot-10']
     for f_str in list_f_str:
         d = []
         with open('dump/' + f_str + '.dump', 'r') as f:
             d = eval(f.readline())
 
         data = [ eval(i) for i in d ]
+        print('File size: %d' %(len(data)))
 
         df = pd.DataFrame.from_records(data, columns=['pwm', 'm1', 'm2', 'm3', 'm4', 'mean'])
         df.to_csv('csv/' + f_str + '.csv', index=False)
--- statistics.py
+++ statistics.py
@@ -3,30 +3,34 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    df = pd.read_csv('csv/robot-0.csv')
+    df_a = pd.read_csv('csv/robot-0.csv')
 
-    data = { 'pwm': list(df.to_dict()['pwm'].values()),
-                'm1': list(df.to_dict()['m1'].values()),
-                'm2': list(df.to_dict()['m2'].values()),
-                'm3': list(df.to_dict()['m3'].values()),
-                'm4': list(df.to_dict()['m4'].values()),
-                'mean': list(df.to_dict()['mean'].values()),
+    data_a = { 'pwm': list(df_a.to_dict()['pwm'].values()),
+                'm1': list(df_a.to_dict()['m1'].values()),
+                'm2': list(df_a.to_dict()['m2'].values()),
+                'm3': list(df_a.to_dict()['m3'].values()),
+                'm4': list(df_a.to_dict()['m4'].values()),
+                'mean': list(df_a.to_dict()['mean'].values()),
             }
 
-    plt.scatter('pwm', 'm1', c='b', data=data)
+    df_b = pd.read_csv('csv/robot-10.csv')
 
-    df = pd.read_csv('csv/robot-10.csv')
-
-    data = { 'pwm': list(df.to_dict()['pwm'].values()),
-                'm1': list(df.to_dict()['m1'].values()),
-                'm2': list(df.to_dict()['m2'].values()),
-                'm3': list(df.to_dict()['m3'].values()),
-                'm4': list(df.to_dict()['m4'].values()),
-                'mean': list(df.to_dict()['mean'].values()),
+    data_b = { 'pwm': list(df_b.to_dict()['pwm'].values()),
+                'm1': list(df_b.to_dict()['m1'].values()),
+                'm2': list(df_b.to_dict()['m2'].values()),
+                'm3': list(df_b.to_dict()['m3'].values()),
+                'm4': list(df_b.to_dict()['m4'].values()),
+                'mean': list(df_b.to_dict()['mean'].values()),
             }
 
-    plt.scatter('pwm', 'm1', c='r', data=data)
-    plt.show()
+
+    m_l = ['m1', 'm2', 'm3', 'm4']
+    for m in m_l:
+        _, ax = plt.subplots()
+        r1 = ax.scatter('pwm', m, c='b', data=data_a)
+        r2 = ax.scatter('pwm', m, c='r', data=data_b)
+        ax.legend((r1, r2), ('truth', 'test'))
+        plt.show()
 
 if __name__ == '__main__':
     main()
